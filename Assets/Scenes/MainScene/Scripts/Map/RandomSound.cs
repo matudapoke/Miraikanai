0a53ec300e585f16fe6466f0c68852dd3c6b0110
matudapoke <165378571+matudapoke@users.noreply.github.com>
2024-05-20 11:55:46 +0900

    a

--- Assets/Scenes/MainScene/Scripts/Map/RandomSound.cs
+++ Assets/Scenes/MainScene/Scripts/Map/RandomSound.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 public class RandomSound : MonoBehaviour
 {
     AudioSource audioSource;
+    AudioSource audioSource1;
     [SerializeField] List<AudioClip> Sounds = new List<AudioClip>();
     [SerializeField] float SoundIntervalMin;
     [SerializeField] float SoundIntervalMax;
@@ -17,6 +18,7 @@ public class RandomSound : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        audioSource1 = GetComponents<AudioSource>()[1];
         StartCoroutine(RandomInterval());
     }
 
@@ -24,7 +26,7 @@ public class RandomSound : MonoBehaviour
     {
         yield return new WaitForSeconds(Random.Range(SoundIntervalMin, SoundIntervalMax));
         float SoundVolume = Random.Range(SoundVolumeMin, SoundIntervalMax);
-        audioSource.PlayOneShot(Sounds[Random.Range(0, Sounds.Count - 1)], SoundVolume);
+        audioSource1.PlayOneShot(Sounds[Random.Range(0, Sounds.Count - 1)], SoundVolume);
         StartCoroutine(RandomInterval());
         if (SpawnPrefab != null)
         {
