1bfe4c5aceb7e325c21d80646767498dfa45b953
matudapoke <165378571+matudapoke@users.noreply.github.com>
2024-12-08 23:34:46 +0900

    1208

--- Assets/Scenes/MainScene/FishBook/FishBook.cs
+++ Assets/Scenes/MainScene/FishBook/FishBook.cs
@@ -6,21 +6,52 @@ using UnityEngine;
 public class FishBook : MonoBehaviour
 {
     [SerializeField] GameObject Cursor;
+
+    bool isOpenFishBook;
+
+    GameObject Canvas_Obj;
+    GameObject Camera_Obj;
+    GameObject Reizi_Obj;
+    void Start()
+    {
+        Canvas_Obj = GameObject.Find("CanvasUI");
+        Camera_Obj = GameObject.Find("Main Camera");
+        Reizi_Obj = GameObject.Find("Reizi");
+    }
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Q))
+        if (Input.GetKeyUp(KeyCode.Q) && !isOpenFishBook)
         {
             StartCoroutine(StartFishBook());
+        }
+        else if (Input.GetKeyUp(KeyCode.Q) && isOpenFishBook)
+        {
+            StartCoroutine (EndFishBook());
         }
     }
     IEnumerator StartFishBook()
     {
-        GameObject CameraObj = GameObject.Find("Main Camera");
-        CameraObj.transform.Find("CameraFade").GetComponent<Fade>().FadeStart(0.5f);
+        isOpenFishBook = true;
+        Reizi_Obj.GetComponent<Animator>().SetBool("IdolA", true);
+        Canvas_Obj.SetActive(false);
+        Camera_Obj.transform.Find("CameraFade").GetComponent<Fade>().FadeStart(0.1f);
         yield return new WaitForSeconds(0.5f);
-        CameraObj.transform.position = new Vector3(-150, 0, 0);
-        CameraObj.transform.Find("CameraFade").GetComponent<Fade>().FadeEnd(0.5f);
-        CameraObj.GetComponent<Cam>().ChangeTarget(Cursor.transform);
+        Camera_Obj.GetComponent<Cam>().ChangeTarget(Cursor.transform);
         yield return new WaitForSeconds(0.5f);
+        Camera_Obj.transform.Find("CameraFade").GetComponent<Fade>().FadeEnd(0.5f);
+        yield return new WaitForSeconds(0.3f);
+        Reizi_Obj.GetComponent<Animator>().SetBool("IdolA", false);
+    }
+    IEnumerator EndFishBook()
+    {
+        isOpenFishBook = false;
+        Camera_Obj.transform.Find("CameraFade").GetComponent<Fade>().FadeStart(0.5f);
+        Reizi_Obj.GetComponent<Animator>().SetBool("IdolA", true);
+        yield return new WaitForSeconds(0.5f);
+        Camera_Obj.GetComponent<Cam>().ChangeTarget(Reizi_Obj.transform);
+        yield return new WaitForSeconds(0.5f);
+        Camera_Obj.transform.Find("CameraFade").GetComponent<Fade>().FadeEnd(0.1f);
+        Canvas_Obj.SetActive(true);
+        Reizi_Obj.GetComponent<Animator>().SetBool("IdolA", false);
     }
 }
--- Assets/Scenes/MainScene/Scripts/Event/Fade.cs
+++ Assets/Scenes/MainScene/Scripts/Event/Fade.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
-    public IEnumerator FadeStart(float FadeTime)
+    public void FadeStart(float FadeTime)
     {
-        SpriteRenderer ActionSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        ActionSpriteRenderer.color = new Color(1, 1, 1, 1);
-        for (float i = 0; i < 1; i += 0.05f)
-        {
-            ActionSpriteRenderer.color = new Color(1, 1, 1, i);
-            yield return new WaitForSeconds(FadeTime / 255);
-        }
+        StartCoroutine(FadeStartCoroutine(FadeTime));
+    }
+    public void FadeEnd(float FadeTime)
+    {
+        StartCoroutine(FadeEndCoroutine(FadeTime));
     }
 
-    public IEnumerator FadeEnd(float FadeTime)
+    IEnumerator FadeStartCoroutine(float FadeTime)
     {
         SpriteRenderer ActionSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        ActionSpriteRenderer.color = new Color(1, 1, 1, 1);
-        for (float i = 1; i > 0; i -= 0.05f)
+        ActionSpriteRenderer.color = new Color(ActionSpriteRenderer.color.r, ActionSpriteRenderer.color.g, ActionSpriteRenderer.color.b, 0);
+        for (float i = 0; i < 1; i += 0.05f)
         {
-            ActionSpriteRenderer.color = new Color(1, 1, 1, i);
+            ActionSpriteRenderer.color = new Color(ActionSpriteRenderer.color.r, ActionSpriteRenderer.color.g, ActionSpriteRenderer.color.b, i);
             yield return new WaitForSeconds(FadeTime / 255);
         }
+        ActionSpriteRenderer.color = new Color(ActionSpriteRenderer.color.r, ActionSpriteRenderer.color.g, ActionSpriteRenderer.color.b, 1);
+    }
+
+    IEnumerator FadeEndCoroutine(float FadeTime)
+    {
+        SpriteRenderer ActionSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        ActionSpriteRenderer.color = new Color(ActionSpriteRenderer.color.r, ActionSpriteRenderer.color.g, ActionSpriteRenderer.color.b, 1);
+        for (float i = 1; i > 0; i -= 0.05f)
+        {
+            ActionSpriteRenderer.color = new Color(ActionSpriteRenderer.color.r, ActionSpriteRenderer.color.g, ActionSpriteRenderer.color.b, i);
+            yield return new WaitForSeconds(FadeTime / 255);
+        }
+        ActionSpriteRenderer.color = new Color(ActionSpriteRenderer.color.r, ActionSpriteRenderer.color.g, ActionSpriteRenderer.color.b, 0);
     }
 }
